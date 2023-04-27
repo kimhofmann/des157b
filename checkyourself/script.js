d282e968f06a054928bf96d7f6392fbdc5f28a86
kimberly_hofmann <kchofmann@ucdavis.edu>
2023-04-26 18:22:51 -0700

    FINALLY I FIXED IT ;U;

--- checkyourself/script.js
+++ checkyourself/script.js
@@ -28,31 +28,56 @@
 
     end1.addEventListener('click', function(){
         targetFace.src = "images/end1.svg"
-        scores.innerhtml = 'please'     
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end1"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end1"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end1"]["sum"]}</p>`
     });
 
     end2.addEventListener('click', function(){
         targetFace.src = "images/end2.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end2"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end2"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end2"]["sum"]}</p>`
     });
 
     end3.addEventListener('click', function(){
         targetFace.src = "images/end3.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end3"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end3"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end3"]["sum"]}</p>`
     });
 
     end4.addEventListener('click', function(){
         targetFace.src = "images/end4.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end4"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end4"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end4"]["sum"]}</p>`
     });
 
     end5.addEventListener('click', function(){
         targetFace.src = "images/end5.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end5"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end5"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end5"]["sum"]}</p>`
     });
 
     end6.addEventListener('click', function(){
         targetFace.src = "images/end6.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">End: ${globalData["end6"]["end"]}</p>`
+        scoreDiv.innerHTML += `<p class="data">Round Score: ${globalData["end6"]["score"]}</p>`
+        scoreDiv.innerHTML += `<p class="data"> Total: ${globalData["end6"]["sum"]}</p>`
     });
 
     totalScore.addEventListener('click', function(){
         targetFace.src = "images/total.svg"
+        scoreDiv.innerHTML = ''
+        scoreDiv.innerHTML = `<p class="data">Final Score: 255</p>`
     });
 
 
