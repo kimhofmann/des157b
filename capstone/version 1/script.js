543296a88b59b6e623682679ff346d76fef29baf
kimberly_hofmann <kchofmann@ucdavis.edu>
2023-05-24 15:06:33 -0700

    updates to animations

--- capstone/version 1/script.js
+++ capstone/version 1/script.js
@@ -5,10 +5,26 @@
 
   let path = anime.path('#demo-svg path')
 
-  var tl = anime.timeline({
-    easing: 'easeOutExpo',
-    duration: 3000
-  });
+  const answerYes = document.querySelector('#yes');
+
+  answerYes.addEventListener('click', function(){
+    var tl = anime.timeline({
+      easing: 'easeOutExpo',
+      duration: 3000
+    });
+    tl
+    .add({
+      targets: '.movingright',
+      translateX: path('x'),
+      translateY: path('y'),
+      delay: anime.stagger(400)
+    })
+    .add({
+      targets: '.movingleft',
+      translateX: -1200,
+      delay: anime.stagger(400)
+    }, '-=3500') // relative offset
+  })
   
   // Add children
   tl
